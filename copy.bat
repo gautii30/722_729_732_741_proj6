28405e1884b724456ea91be44e186b9856979c6c
vivek-698 <85101944+vivek-698@users.noreply.github.com>
2023-04-27 21:27:13 +0530

    Delete copy.bat

--- copy.bat
+++
@@ -1,5 +0,0 @@
-copy C:\Users\Abishek\Desktop\cc\cc_project\uc1\.env C:\Users\Abishek\.jenkins\workspace\cc-project-pipeline\uc1
-copy C:\Users\Abishek\Desktop\cc\cc_project\uc2\.env C:\Users\Abishek\.jenkins\workspace\cc-project-pipeline\uc2
-copy C:\Users\Abishek\Desktop\cc\cc_project\uc3\.env C:\Users\Abishek\.jenkins\workspace\cc-project-pipeline\uc3
-
-
