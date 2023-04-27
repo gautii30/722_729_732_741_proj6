d6ee64ee4cda6a86784c9d1a909c6279a70e99b6
rhea-19 <93151142+rhea-19@users.noreply.github.com>
2023-04-27 13:21:30 -0400

    Add files via upload

---
+++ copy.bat
@@ -0,0 +1,3 @@
+copy C:\ProgramData\Jenkins\.jenkins\workspace\cc_demo\uc1.env C:\ProgramData\Jenkins\.jenkins\workspace\cc_demo\uc1
+copy C:\ProgramData\Jenkins\.jenkins\workspace\cc_demo\uc2.env C:\ProgramData\Jenkins\.jenkins\workspace\cc_demo\uc2
+copy C:\ProgramData\Jenkins\.jenkins\workspace\cc_demo\uc3.env C:\ProgramData\Jenkins\.jenkins\workspace\cc_demo\uc3
