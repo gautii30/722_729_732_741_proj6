b35078f3bf1176811de474651d25833e0291127c
gautii30 <sarafgautam2002@gmail.com>
2024-04-23 03:25:02 +0530

    update bat file

--- copy.bat
+++ copy.bat
@@ -1,3 +1,3 @@
-copy D:\6thsem\Cloud_computing\Project\upshop\uc1\.env C:\ProgramData\Jenkins\.jenkins\workspace\cc_demo\uc1
-copy D:\6thsem\Cloud_computing\Project\upshop\uc1\.env C:\ProgramData\Jenkins\.jenkins\workspace\cc_demo\uc2
-copy D:\6thsem\Cloud_computing\Project\upshop\uc1\.env C:\ProgramData\Jenkins\.jenkins\workspace\cc_demo\uc3
+cp /Users/gautam/Desktop/sem-6/cloud\ computing/ccprojf/E-commerce-microservices-application/uc1/.env /Users/gautam/.jenkins/workspace/cc_demo/uc1
+cp /Users/gautam/Desktop/sem-6/cloud\ computing/ccprojf/E-commerce-microservices-application/uc1/.env /Users/gautam/.jenkins/workspace/cc_demo/uc2
+cp /Users/gautam/Desktop/sem-6/cloud\ computing/ccprojf/E-commerce-microservices-application/uc1/.env /Users/gautam/.jenkins/workspace/cc_demo/uc3
