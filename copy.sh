a6b2980df89628185918ef8088335b7840ebe0ec
gautii30 <sarafgautam2002@gmail.com>
2024-04-23 04:14:53 +0530

    update bat file

--- copy.sh
+++ copy.sh
@@ -1,3 +1,3 @@
-cp /Users/gautam/Desktop/sem-6/cloud\ computing/ccprojf/E-commerce-microservices-application/uc1/.env /Users/gautam/.jenkins/workspace/cc_demo/uc1
-cp /Users/gautam/Desktop/sem-6/cloud\ computing/ccprojf/E-commerce-microservices-application/uc1/.env /Users/gautam/.jenkins/workspace/cc_demo/uc2
-cp /Users/gautam/Desktop/sem-6/cloud\ computing/ccprojf/E-commerce-microservices-application/uc1/.env /Users/gautam/.jenkins/workspace/cc_demo/uc3
+cp '/Users/gautam/Desktop/sem-6/cloud\ computing/ccprojf/E-commerce-microservices-application/uc1/.env' /Users/gautam/.jenkins/workspace/cc_demo/uc1
+cp '/Users/gautam/Desktop/sem-6/cloud\ computing/ccprojf/E-commerce-microservices-application/uc2/.env' /Users/gautam/.jenkins/workspace/cc_demo/uc2
+cp '/Users/gautam/Desktop/sem-6/cloud\ computing/ccprojf/E-commerce-microservices-application/uc3/.env' /Users/gautam/.jenkins/workspace/cc_demo/uc3
